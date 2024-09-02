6c9bd676f7264dffc977883cf94144be747cc3ca
Cederix <s1212611@student.windesheim.nl>
2024-09-02 20:19:51 +0200

    Assignment 2

--- Week1Opdracht2/Opdracht 2.c
+++ Week1Opdracht2/Opdracht 2.c
@@ -3,15 +3,10 @@
 
 void main()
 {
-
 	int a, b, c;
 	printf("Voer 3 integers in: ");
 	scanf("%d", &a);
 	scanf("%d", &b);
 	scanf("%d", &c);
 	printf("Uw gemiddelde is %d", (a + b + c) / 3);
-
-
-
-
 }
