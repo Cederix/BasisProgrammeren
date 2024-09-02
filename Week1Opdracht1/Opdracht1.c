feac5a4d6fa857cf422c62a4105018b1ad4bd623
Cederix <s1212611@student.windesheim.nl>
2024-09-02 19:55:36 +0200

    Debug_Fix

--- Week1Opdracht1/Opdracht1.c
+++ Week1Opdracht1/Opdracht1.c
@@ -1,14 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 
 void main()
 {
 	int leeftijd;
-
-	printf("Voer hier uw leeftijd in: \n");
 	
+	printf("Voer uw leeftijd in: ");
 	scanf("%d", &leeftijd);
-	
-	printf("U bent %d jaar oud", leeftijd);
+	printf("U bent %d jaar oud",leeftijd);
 }
