69282dc043d7368b0391f7bba5426805f15f985c
Cederix <s1212611@student.windesheim.nl>
2024-09-02 20:30:35 +0200

    Assignment3

---
+++ Week1Opdracht3/Opdracht3.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+	int a, b;
+	printf("Voer 2 integers in");
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	if (a > b)
+		printf("%d is groter", a);
+	else
+		printf("%d is groter", b);
+}
