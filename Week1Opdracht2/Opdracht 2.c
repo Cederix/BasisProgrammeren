e2a38209fca0171c917b06871fdf73fcff72a279
Cederix <s1212611@student.windesheim.nl>
2024-09-02 20:18:19 +0200

    Assignment2_ready

---
+++ Week1Opdracht2/Opdracht 2.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+
+	int a, b, c;
+	printf("Voer 3 integers in: ");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	printf("Uw gemiddelde is %d", (a + b + c) / 3);
+
+
+
+
+}
