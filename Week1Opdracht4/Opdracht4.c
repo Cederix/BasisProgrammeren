3b62b815feb0bbf026b2d6dd5d0d5edd667e9ddc
Cederix <s1212611@student.windesheim.nl>
2024-09-02 20:49:04 +0200

    Assignment4_prog

---
+++ Week1Opdracht4/Opdracht4.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*vrij lastig kom zo terug*/
+void main()
+{
+	int a, b, c;
+	printf("Voer 3 integers in");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+
+	if (a > b, a > c)
+	{
+		printf("%d is groter", a);
+	}
+	else (b > a);
+	{
+		printf("%d is groter", b);
+	}
+	
+
+         
+
+
+}
