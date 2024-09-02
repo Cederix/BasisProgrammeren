23b2d5dab3c5c756aa1f7175c7a931d9575c62ab
Cederix <s1212611@student.windesheim.nl>
2024-09-02 19:19:14 +0200

    Assignment_2_start

---
+++ Week1Opdracht1/Opdracht1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void main()
+{
+	int leeftijd;
+
+	printf("Voer hier uw leeftijd in: \n");
+	
+	scanf("%d", &leeftijd);
+	
+	printf("U bent %d jaar oud", leeftijd);
+}
