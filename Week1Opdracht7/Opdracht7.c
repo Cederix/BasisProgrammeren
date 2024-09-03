f6b6488d650858e168598ba3a44466e493fa37f2
Cederix <s1212611@student.windesheim.nl>
2024-09-03 20:11:32 +0200

    Assignment7

---
+++ Week1Opdracht7/Opdracht7.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define pi 3.14159265358979323846
+#include <stdio.h>
+
+void main()
+{
+	float r;
+	printf("Yo ass finna put in a numba:");
+	scanf("%f", &r);
+	printf("de omtrek is %f", r*2*pi);
+	printf("\nde oppervlakte is %f", pi*r*r);
+
+
+}
