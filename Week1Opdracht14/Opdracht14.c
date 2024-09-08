3db0723833d3fbf425aa290d04ce12d8936df994
Cederix <s1212611@student.windesheim.nl>
2024-09-08 21:49:18 +0200

    Opdracht14

--- Week1Opdracht14/Opdracht14.c
+++ Week1Opdracht14/Opdracht14.c
@@ -3,13 +3,18 @@
 
 void main()
 {
-	int a, c;
-	printf("Voer een integers in: ");
+	int a;
+	int c;
+	printf("Voer een integers in: \n");
 	scanf("%d", &a);
-	printf("Wilt u dit getal kwadrateren of wilt u de derde machtsheffing?\n Kies uit kwadrateren (1) of derde_machtsheffing (2)");
+	printf("Uw getal is %d\nWilt u dit getal kwadrateren of wilt u de derde machtsheffing?\n Kies uit kwadrateren (1) of derde machtsheffing (2)", a);
 	scanf("%d", &c);
-	if (c == "1")
-		printf("Het gekwadrateerd getal is %d", a * a);
-	else if (c == "2")
-		printf("De derde machtsheffing van het getal is %d", a * a * a);
+	if (c == 1)
+	{
+		printf("\nHet gekwadrateerd getal is %d", a * a);
+	}
+	else if (c == 2)
+	{
+		printf("\nDe derde machtsheffing van het getal is %d", a * a * a);
+	}
 }
