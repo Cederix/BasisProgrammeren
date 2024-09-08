f49e300f778eb0b83fc926379e21284b4e69f73e
Cederix <s1212611@student.windesheim.nl>
2024-09-08 21:02:28 +0200

    Assignment12

---
+++ Week1Opdracht12/Opdracht12.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+	int a;
+	printf("Voer een integers in: ");
+	scanf("%d", &a);
+	printf("Het gekwadrateerd getal is %d",a* a);
+
+}
