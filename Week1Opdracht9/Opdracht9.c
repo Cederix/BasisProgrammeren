b4688a1a5a057cb8e81b95d4f5b62ca276fe9cb0
Cederix <s1212611@student.windesheim.nl>
2024-09-08 14:44:17 +0200

    Assignment9

---
+++ Week1Opdracht9/Opdracht9.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+	float s;
+	printf("Voer een snelheid (km/h) in: ");
+	scanf("%f", &s);
+	printf("De snelheid is %f m/s", s/3.6);
+
+
+}
