5ce8522aed52e11796ac058a145415279a4c6896
Cederix <s1212611@student.windesheim.nl>
2024-09-23 09:53:14 +0200

    Week 2 af!

--- Week2/Opdracht9/Opdracht9.c
+++ Week2/Opdracht9/Opdracht9.c
@@ -2,5 +2,19 @@
 #include <stdio.h>
 void main()
 {
+	int a, b;
+	double c, d, e;
+	printf("Voer het aantal daluren per dag in: ");
+	scanf("%d", &a);
+	printf("Voer het aantal piekuren per dag in: ");
+	scanf("%d", &b);
+	c = a * 365 * 0.40;
+	d = b * 365 * 0.46;
 
+	if (a+b==24)
+	{
+		e = c + d + 80;
+		printf("%.2f euro",e);
+
+	}
 }
